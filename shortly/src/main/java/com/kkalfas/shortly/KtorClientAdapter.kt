297a3a96a4e3c59fd268ddfc08261b6f1378603e
Kamil Kalfas <kamilkalfas@gmail.com>
2022-11-19 15:14:02 +0100

    - added ktor dependencies - implemented http client provider

---
+++ shortly/src/main/java/com/kkalfas/shortly/KtorClientAdapter.kt
@@ -0,0 +1,54 @@
+package com.kkalfas.shortly
+
+import io.ktor.client.*
+import io.ktor.client.engine.*
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.logging.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+
+/**
+ * Abstraction layer allowing to change [HttpClient] implementation
+ */
+interface KtorHttpClientProvider {
+    fun provide(engine: HttpClientEngine) : HttpClient
+
+    class Impl : KtorHttpClientProvider {
+        override fun provide(engine: HttpClientEngine): HttpClient {
+            return HttpClient(engine) {
+                install(UserAgent) { agent = "ktor" }
+                installDefaultHeaders()
+                installJSONSerialization()
+                if (BuildConfig.DEBUG) {
+                    install(Logging) {
+                        level = LogLevel.ALL
+                    }
+                }
+            }
+        }
+    }
+}
+
+/**
+ * Extracted to have same implementation if MockEngine used
+ */
+fun HttpClientConfig<*>.installJSONSerialization() {
+    install(ContentNegotiation) {
+        json(
+            Json {
+                prettyPrint = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            }
+        )
+    }
+}
+
+fun HttpClientConfig<*>.installDefaultHeaders() {
+    install(DefaultRequest) {
+        header(HttpHeaders.ContentType, ContentType.Application.Json)
+    }
+}
