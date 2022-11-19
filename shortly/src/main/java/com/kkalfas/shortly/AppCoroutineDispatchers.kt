19054fb69aa23e8dbdc4da1414e830353838fd57
Kamil Kalfas <kamilkalfas@gmail.com>
2022-11-19 15:14:37 +0100

    - added dispatchers wrapper

---
+++ shortly/src/main/java/com/kkalfas/shortly/AppCoroutineDispatchers.kt
@@ -0,0 +1,9 @@
+package com.kkalfas.shortly
+
+import kotlinx.coroutines.CoroutineDispatcher
+
+data class AppCoroutineDispatchers(
+    val io: CoroutineDispatcher,
+    val computation: CoroutineDispatcher,
+    val main: CoroutineDispatcher
+)
