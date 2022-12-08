da81a161c8e96e7bc72205c770480cf278d148f3
Kamil Kalfas <kamilkalfas@gmail.com>
2022-12-08 17:38:27 +0100

    Added compose colors and theme.

---
+++ shortly/src/main/java/com/kkalfas/shortly/presentation/theme/Colors.kt
@@ -0,0 +1,16 @@
+package com.kkalfas.shortly.presentation.theme
+
+import androidx.compose.material.Colors
+import androidx.compose.material.lightColors
+import androidx.compose.ui.graphics.Color
+
+val LightThemeColors = lightColors(
+    primary = Color(0xFF2acfcf),
+    primaryVariant = Color(0xFF3B3054),
+    secondary = Color(0xFF3B3054),
+    background = Color.White,
+
+)
+
+val Colors.backgroundSecondary: Color
+    get() = Color(0xFFF0F1F6)
